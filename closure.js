79018498e8a26f367b81c531541e45b92bc3a471
11-1111 <153610684+11-1111@users.noreply.github.com>
2024-08-16 15:09:00 +0300

    state persistence between functions

---
+++ closure.js
@@ -0,0 +1,18 @@
+//Allows functions to access variables in the parent scope
+//The function can access the variable count
+//State persistence between function calls ie it maintains the updated value of count on each call
+
+function createCounter(n){
+    let count=n;
+    return function(){  
+        let x=count;
+    count++;
+    return x;
+    }
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter()); 
+console.log(counter());
+console.log(counter());
