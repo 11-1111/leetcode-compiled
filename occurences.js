3035b69536d2d9c21e6c614a92fc8230f303c508
11-1111 <153610684+11-1111@users.noreply.github.com>
2024-08-20 16:25:02 +0300

    sorting arrays

--- occurences.js
+++ occurences.js
@@ -7,11 +7,21 @@ arr.forEach(person => {
    } else {
       occurrences[person] = 1;
    }
+   const total = occurrences[person];
 });
 
-const arranged = occurrences.sort((a, b)=> )
-console.log(occurrences);
+const score = 0;
+const total = arr.map(person =>{
 
+    if (occurrences[person]) {
+      occurrences[person]++;
+   } else {
+      occurrences[person] = 1;
+   }
+   score += occurrences[person];
+
+})
+console.log(score);
 
 
 // 
