eebe56c6cb927625b9f67f662136ed4ab3355975
11-1111 <153610684+11-1111@users.noreply.github.com>
2024-08-21 13:16:48 +0300

    changes in occurences

--- occurences.js
+++ occurences.js
@@ -1,5 +1,5 @@
 const occurrences = {};  // Renamed for correct spelling
-const arr = ['ivy', 'ivy', 'ivy', 'james','ivy', 'kevin'];  // Use quotes for string literals
+const arr = ['mary', 'ivy', 'ivy', 'james','ivy', 'kevin'];  // Use quotes for string literals
 
 arr.forEach(person => {
    if (occurrences[person]) {
@@ -10,30 +10,3 @@ arr.forEach(person => {
    
 });
 
-// const score = 0;
-// const total = arr.map(person =>{
-
-//     if (occurrences[person]) {
-//       occurrences[person]++;
-//    } else {
-//       occurrences[person] = 1;
-//    }
-//    score += occurrences[person];
-
-// })
-// console.log(score);
-
-
-// 
-/*const occurrences = {};  
-const arr = ['ivy', 'ivy', 'ivy', 'james','ivy', 'kevin'];  
-
-arr.forEach(person => {
-   if (occurrences.person) {
-      occurrences.person++;
-   } else {
-      occurrences.person = 1;
-   }
-});
-
-console.log(occurrences);
